c9dabe715ce7951b7c9c9113b1e6d36c2cfbd5ec
Rick V <rick@snowlight.net>
2020-01-17 20:00:01 -0600

    BUG: sun studio hates VIA padlock extension style fix (expand space to tab)

--- src/bech32/bech32.c
+++ src/bech32/bech32.c
@@ -27,28 +27,28 @@
 static const char* charset = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
 
 static const int8_t charset_rev[128] = {
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
-    -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
-     1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
-    -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
-     1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
+	-1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+	 1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
+	-1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+	 1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
 };
 
 static uint32_t bech32_polymod_step(pre)
 uint32_t pre;
 {
-    uint8_t b;
+	uint8_t b;
 
-    b = pre >> 25;
-    return ((pre & 0x1FFFFFF) << 5) ^
-        (-((b >> 0) & 1) & 0x3b6a57b2UL) ^
-        (-((b >> 1) & 1) & 0x26508e6dUL) ^
-        (-((b >> 2) & 1) & 0x1ea119faUL) ^
-        (-((b >> 3) & 1) & 0x3d4233ddUL) ^
-        (-((b >> 4) & 1) & 0x2a1462b3UL);
+	b = pre >> 25;
+	return ((pre & 0x1FFFFFF) << 5) ^
+		   (-((b >> 0) & 1) & 0x3b6a57b2UL) ^
+		   (-((b >> 1) & 1) & 0x26508e6dUL) ^
+		   (-((b >> 2) & 1) & 0x1ea119faUL) ^
+		   (-((b >> 3) & 1) & 0x3d4233ddUL) ^
+		   (-((b >> 4) & 1) & 0x2a1462b3UL);
 }
 
 bech32_encode(output, hrp, data, data_len)
@@ -57,43 +57,46 @@ const char* hrp;
 const uint8_t* data;
 size_t data_len;
 {
-    uint32_t chk;
-    size_t i;
-    int ch;
+	uint32_t chk;
+	size_t i;
+	int ch;
 
-    chk = 1;
-    i = 0;
-    while (hrp[i] != 0) {
-        ch = hrp[i];
-        if (ch < 33 || ch > 126)
-            return 0;
+	chk = 1;
+	i = 0;
+	while (hrp[i] != 0)
+	{
+		ch = hrp[i];
+		if (ch < 33 || ch > 126)
+			return 0;
 
-        /* Do not check for case, we always pass single-case here */
-        /*if (ch >= 'a' && ch <= 'z') return 0;*/
-        chk = bech32_polymod_step(chk) ^ (ch >> 5);
-        ++i;
-    }
-    if (i + 7 + data_len > 90) return 0;
-    chk = bech32_polymod_step(chk);
-    while (*hrp != 0) {
-        chk = bech32_polymod_step(chk) ^ (*hrp & 0x1f);
-        *(output++) = *(hrp++);
-    }
-    *(output++) = '1';
-    for (i = 0; i < data_len; ++i) {
-        if (*data >> 5) return 0;
-        chk = bech32_polymod_step(chk) ^ (*data);
-        *(output++) = charset[*(data++)];
-    }
-    for (i = 0; i < 6; ++i) {
-        chk = bech32_polymod_step(chk);
-    }
-    chk ^= 1;
-    for (i = 0; i < 6; ++i) {
-        *(output++) = charset[(chk >> ((5 - i) * 5)) & 0x1f];
-    }
-    *output = 0;
-    return 1;
+		/* Do not check for case, we always pass single-case here */
+		/*if (ch >= 'a' && ch <= 'z') return 0;*/
+		chk = bech32_polymod_step(chk) ^ (ch >> 5);
+		++i;
+	}
+	if (i + 7 + data_len > 90)
+		return 0;
+	chk = bech32_polymod_step(chk);
+	while (*hrp != 0)
+	{
+		chk = bech32_polymod_step(chk) ^ (*hrp & 0x1f);
+		*(output++) = *(hrp++);
+	}
+	*(output++) = '1';
+	for (i = 0; i < data_len; ++i)
+	{
+		if (*data >> 5)
+			return 0;
+		chk = bech32_polymod_step(chk) ^ (*data);
+		*(output++) = charset[*(data++)];
+	}
+	for (i = 0; i < 6; ++i)
+		chk = bech32_polymod_step(chk);
+	chk ^= 1;
+	for (i = 0; i < 6; ++i)
+		*(output++) = charset[(chk >> ((5 - i) * 5)) & 0x1f];
+	*output = 0;
+	return 1;
 }
 
 bech32_decode(hrp, data, data_len, input)
@@ -102,62 +105,59 @@ uint8_t* data;
 size_t* data_len;
 const char* input;
 {
-    uint32_t chk;
-    size_t i, input_len, hrp_len;
-    int have_lower, have_upper, ch, v;
+	uint32_t chk;
+	size_t i, input_len, hrp_len;
+	int have_lower, have_upper, ch, v;
 
-    chk = 1;
-    input_len = strlen(input);
-    have_lower = have_upper = 0;
-    if (input_len < 8 || input_len > 90) {
-        return 0;
-    }
-    *data_len = 0;
-    while (*data_len < input_len && input[(input_len - 1) - *data_len] != '1') {
-        ++(*data_len);
-    }
-    hrp_len = input_len - (1 + *data_len);
-    if (1 + *data_len >= input_len || *data_len < 6) {
-        return 0;
-    }
-    *(data_len) -= 6;
-    for (i = 0; i < hrp_len; ++i) {
-        ch = input[i];
-        if (ch < 33 || ch > 126) {
-            return 0;
-        }
-        if (ch >= 'a' && ch <= 'z') {
-            have_lower = 1;
-        } else if (ch >= 'A' && ch <= 'Z') {
-            have_upper = 1;
-            ch = (ch - 'A') + 'a';
-        }
-        hrp[i] = ch;
-        chk = bech32_polymod_step(chk) ^ (ch >> 5);
-    }
-    hrp[i] = 0;
-    chk = bech32_polymod_step(chk);
-    for (i = 0; i < hrp_len; ++i) {
-        chk = bech32_polymod_step(chk) ^ (input[i] & 0x1f);
-    }
-    ++i;
-    while (i < input_len) {
-        v = (input[i] & 0x80) ? -1 : charset_rev[(int)input[i]];
-        if (input[i] >= 'a' && input[i] <= 'z') have_lower = 1;
-        if (input[i] >= 'A' && input[i] <= 'Z') have_upper = 1;
-        if (v == -1) {
-            return 0;
-        }
-        chk = bech32_polymod_step(chk) ^ v;
-        if (i + 6 < input_len) {
-            data[i - (1 + hrp_len)] = v;
-        }
-        ++i;
-    }
-    if (have_lower && have_upper) {
-        return 0;
-    }
-    return chk == 1;
+	chk = 1;
+	input_len = strlen(input);
+	have_lower = have_upper = 0;
+	if (input_len < 8 || input_len > 90)
+		return 0;
+	*data_len = 0;
+	while (*data_len < input_len && input[(input_len - 1) - *data_len] != '1')
+		++(*data_len);
+	hrp_len = input_len - (1 + *data_len);
+	if (1 + *data_len >= input_len || *data_len < 6)
+		return 0;
+	*(data_len) -= 6;
+	for (i = 0; i < hrp_len; ++i)
+	{
+		ch = input[i];
+		if (ch < 33 || ch > 126)
+			return 0;
+		if (ch >= 'a' && ch <= 'z')
+			have_lower = 1;
+		else if (ch >= 'A' && ch <= 'Z')
+		{
+			have_upper = 1;
+			ch = (ch - 'A') + 'a';
+		}
+		hrp[i] = ch;
+		chk = bech32_polymod_step(chk) ^ (ch >> 5);
+	}
+	hrp[i] = 0;
+	chk = bech32_polymod_step(chk);
+	for (i = 0; i < hrp_len; ++i)
+		chk = bech32_polymod_step(chk) ^ (input[i] & 0x1f);
+	++i;
+	while (i < input_len)
+	{
+		v = (input[i] & 0x80) ? -1 : charset_rev[(int)input[i]];
+		if (input[i] >= 'a' && input[i] <= 'z')
+			have_lower = 1;
+		if (input[i] >= 'A' && input[i] <= 'Z')
+			have_upper = 1;
+		if (v == -1)
+			return 0;
+		chk = bech32_polymod_step(chk) ^ v;
+		if (i + 6 < input_len)
+			data[i - (1 + hrp_len)] = v;
+		++i;
+	}
+	if (have_lower && have_upper)
+		return 0;
+	return chk == 1;
 }
 
 static convert_bits(out, outlen, outbits, in, inlen, inbits, pad)
@@ -166,28 +166,30 @@ const uint8_t* in;
 size_t* outlen, inlen;
 int outbits, inbits, pad;
 {
-    uint32_t val, maxv;
-    int bits;
+	uint32_t val, maxv;
+	int bits;
 
-    val = 0;
-    bits = 0;
-    maxv = (((uint32_t)1) << outbits) - 1;
-    while (inlen--) {
-        val = (val << inbits) | *(in++);
-        bits += inbits;
-        while (bits >= outbits) {
-            bits -= outbits;
-            out[(*outlen)++] = (val >> bits) & maxv;
-        }
-    }
-    if (pad) {
-        if (bits) {
-            out[(*outlen)++] = (val << (outbits - bits)) & maxv;
-        }
-    } else if (((val << (outbits - bits)) & maxv) || bits >= inbits) {
-        return 0;
-    }
-    return 1;
+	val = 0;
+	bits = 0;
+	maxv = (((uint32_t)1) << outbits) - 1;
+	while (inlen--)
+	{
+		val = (val << inbits) | *(in++);
+		bits += inbits;
+		while (bits >= outbits)
+		{
+			bits -= outbits;
+			out[(*outlen)++] = (val >> bits) & maxv;
+		}
+	}
+	if (pad)
+	{
+		if (bits)
+			out[(*outlen)++] = (val << (outbits - bits)) & maxv;
+	}
+	else if (((val << (outbits - bits)) & maxv) || bits >= inbits)
+		return 0;
+	return 1;
 }
 
 age_key_encode(output, hrp, key, key_len)
